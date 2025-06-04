0a2647470e96197aff125d3ed6d2f5b35507a5ab
Tomal Joy <159746402+Tomal-Paul-Joy@users.noreply.github.com>
2025-06-04 13:10:36 +0600

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -54,4 +54,127 @@ vector<int> primeFactors(int n) {
 }
 ll gcd(ll a , ll b ) { return __gcd(a,b); }
 ll lcm(ll a , ll b ) { return (a/gcd(a,b)) * b; }
+void noOfDivisorsOfaNumber() { 
+    
+    
+    int n;cin>>n ;   
+    for(int i = 1; i*i<=n;  i++ ) { 
+    
+    
+         if (n%i==0) {
+         	 
+         	 cout<<i<<" "<<n/i<<endl ; 
+         	 if (i*i!=n)cout<<n/i<<" "<<i<<endl ;
+         	
+         }
+         
+    
+    
+    
+    }
+
+
+
+}
+void sumofdivisorstilln() { 
+     
+     int n; 
+     cin>>n; 
+     int sum =  0; 
+     for(int i = 1 ; i*i<=n ; i++ ) { 
+     
+          int val = (n/i) - i ; 
+          sum+=2*val; 
+     
+         
+     
+     }
+     sum+=sqrt(n);
+     cout<<sum<<endl ; 
+     
+ 
+ 
+ }
+ int sumOfDivisorsforOneNumber(int n) {
+	
+	vector<int>primes; 
+	sieve(n);
+	primes.push_back(2); 
+	 
+	for(int i = 3  ; i<=n ; i+=2 ) { 
+	
+	    if (isPrime[i]) primes.push_back(i); 
+	    
+	
+	}
+	
+	int sod = 1 ;
+	
+	for(auto it: primes ) { // O(sqrt(n)/ln(sqrt(n)))
+	
+	    if(1LL * it *it > n ) break; 
+	    
+	    
+	    int sum = 1 ; 
+	    int a = 1 ; 
+	    
+	    while(n%it==0) {// + O(log(2base)(n))
+	    
+	       a*=it ; 
+	       
+	       sum+=a; 
+	       
+	    	
+	       n/=it; 
+	    } 
+	    sod*=sum; 
+	     
+	
+	}
+	if (n>1) sod*=(n+1); 
+	return sod   ; 
+	
+} 
+int eulerFuctionToFindHowManyCoprimesofaNumbergcdequalTo1(int n) {
+	
+	//n = p1^a1*p2^a2*....*pk^ak;
+	//euler(n) = gcd(a,n) = 1 [1<= a <=n] = n*((p1-1)/p1)*((p2-1)/p2) *....*((pk-1)/pk)
+	
+	vector<int>primes; 
+	sieve(n);
+	primes.push_back(2); 
+	 
+	for(int i = 3  ; i<=n ; i+=2 ) { 
+	
+	    if (isPrime[i]) primes.push_back(i); 
+	    
+	
+	}
+	
+	int sum = n; 
+	
+	for(auto it: primes ) { // O(sqrt(n)/ln(sqrt(n)))
+	
+	    if(1LL * it *it > n ) break; 
+	    
+	    if(n%it==0) { 
+	        while(n%it==0) {// + O(log(2base)(n))
+	    	n/=it; 
+	       } 
+	       
+	       sum/=it ;
+	       sum*=(it-1); 
+	    
+	    }
+	   
+	     
+	
+	}
+	if (n>1) {
+		sum/=n ; 
+	} 
+	return sum*=(n-1) ; 
+	
+} 
+ 
  
